2cef0d200a519f9fb8bab748b58f10e39942dd5a
oscar <oscar.goodwin@gmail.com>
2021-10-07 17:49:04 +0800

    Onclick lister in XML to navigate directly to fragment_welcome using databinding

--- app/src/main/java/com/udacity/shoestore/LoginViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/LoginViewModel.kt
@@ -1,10 +1,17 @@
 package com.udacity.shoestore
 
+import android.view.View
 import androidx.lifecycle.ViewModel
+import androidx.navigation.Navigation
 
 //Class needs to extend ViewModel
 class LoginViewModel : ViewModel() {
 
 
+    fun navigateToWelcomeFragment(view: View) {
+        val action = LoginFragmentDirections.actionLoginFragmentToWelcomeFragment()
+        Navigation.findNavController(view).navigate(action)
+
+    }
 
 }
